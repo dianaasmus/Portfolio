4a719e0a2fb1d2b94440ccd38573948e42074b7b
Diana Asmus <asmus.diana@icloud.com>
2023-09-05 01:17:09 +0200

    header + favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StartpageComponent } from './startpage/startpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StartpageComponent
   ],
   imports: [
     BrowserModule,
