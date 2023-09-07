0998fb034954b2707175ebb43c5a9f3cc570bf19
Diana Asmus <asmus.diana@icloud.com>
2023-09-07 18:54:14 +0200

    skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  iconData = [
+    { src: '../../assets/img/icons/html_css.png', alt: 'HTML/CSS' },
+    { src: '../../assets/img/icons/javaScript.png', alt: 'JavaScript' },
+    { src: '../../assets/img/icons/rest-api.png', alt: 'Rest API' },
+    { src: '../../assets/img/icons/figma.png', alt: 'Figma' },
+    { src: '../../assets/img/icons/git.png', alt: 'GIT' },
+    { src: '../../assets/img/icons/scrum.png', alt: 'Scrum', style: 'height: 96px;' },
+    { src: '../../assets/img/icons/angular.png', alt: 'Angular' },
+    { src: '../../assets/img/icons/typescript.png', alt: 'TypeScript' },
+    { src: '../../assets/img/icons/firebase.png', alt: 'Firebase' }
+  ];
 }
