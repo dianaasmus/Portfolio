813f834cd5813b084bcbb6fe5a241b930f289439
Diana Asmus <asmus.diana@icloud.com>
2023-09-27 00:07:19 +0200

    scroll Intersection Observer

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-me.component.scss']
 })
 export class ContactMeComponent {
+  @ViewChild('elementToObserve') elementToObserve!: ElementRef;
+  private observer: any;
+  @Input() showArrow: boolean = false;
 
+
+  constructor(private renderer: Renderer2) { }
+  
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(this.callback, {
+      root: null, // Das Wurzelelement (null entspricht dem Viewport)
+      rootMargin: '0px',
+      threshold: 0.5 // Schwellenwert für Sichtbarkeit (0 bis 1)
+    });
+
+    observer.observe(this.elementToObserve.nativeElement);
+  }
+
+  callback(entries: IntersectionObserverEntry[]) {
+    const scrollNote = document.querySelector('.scroll-note');
+    const scrollUp = document.querySelector('.arrow-up');
+
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+  
+        scrollNote?.classList.add('d-none');
+        scrollUp?.classList.remove('d-none');
+        this.showArrow = true;        
+
+      } else {
+
+        this.showArrow = false;
+        scrollNote?.classList.remove('d-none');
+        scrollUp?.classList.add('d-none');
+
+      }
+    });
+  }
+  
+  ngOnDestroy() {
+    // Observer aufräumen
+    this.observer.disconnect();
+  }
+  
 }
--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-startscreen',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./startscreen.component.scss']
 })
 export class StartscreenComponent {
+  @Input() showArrow: boolean = false;
 
 }
