8a14dc2dd3369cb523d72a6a16991916de2c8fb5
Diana Asmus <asmus.diana@icloud.com>
2023-10-02 06:13:20 +0200

    send_mail.php (Server)

---
+++ src/assets/send_mail.php
@@ -0,0 +1,53 @@
+<?php
+
+########### CONFIG ###############
+
+$recipient = 'asmus.diana@icloud.com'; # Bitte hier deine E-Mail angeben
+$redirect = 'success.html';
+
+########### CONFIG END ###########
+
+########### Instruction ###########   
+#
+#   This script has been created to send an email to the $recipient
+#   
+#  1) Upload this file to your FTP Server
+#  2) Send a POST request to this file, including
+#     [name] The name of the sender (Absender)
+#     [message] Message that should be send to you
+#     [email] The sender's email address
+#
+##################################
+
+###############################
+#
+#        DON'T CHANGE ANYTHING FROM HERE!
+#
+#        Ab hier nichts mehr ändern!
+#
+###############################
+
+if (empty($recipient)) {
+    die("Bitte geben Sie die E-Mail-Adresse in Zeile 5 an.");
+}
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case ("OPTIONS"): //Allow preflighting to take place.
+        header("Access-Control-Allow-Headers: content-type");
+        exit;
+    case ("POST"): //Send the email;
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+
+        $subject = "Portfolio message";
+        $headers = "From: " . $name . " <" . $email . ">";
+
+        mail($recipient, $subject, $_POST['message'], $headers);
+        header("Location: " . $redirect); 
+
+        break;
+    default: //Reject any non POST or OPTIONS requests.
+        header("Allow: POST", true, 405);
+        exit;
+}
